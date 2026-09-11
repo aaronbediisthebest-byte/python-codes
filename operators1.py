fa3fb520d26068e9cf76c9ea94f81ed9b293e761
aaronbediisthebest-byte <aaron.bedi.isthebest@gmail.com>
2026-09-11 18:28:44 +0800

    Add files via upload

---
+++ operators1.py
@@ -0,0 +1,14 @@
+field1=120
+field2=30
+field3=90
+total=field1+field2+field3
+avg=total/3
+print("total:",total)
+print("avg:",avg)
+earn=15
+total__earn=total*earn
+print("total earnings:",total__earn)
+last=200
+print("was it better last year?",last>total)
+total-=15
+print("Total harvest after removing damaged ones:",total)

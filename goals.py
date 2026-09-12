5011e2edd8abc4e8bb539b0228cd728ef4167164
aaronbediisthebest-byte <aaron.bedi.isthebest@gmail.com>
2026-09-12 20:02:55 +0800

    Add files via upload

---
+++ goals.py
@@ -0,0 +1,4 @@
+goals=input("enter your goals:")
+print("keep percevering your aim to",goals)
+train=input("how do you train yourself to be good at this goal?",)
+print("keep it up,your parents must be very proud of you!")
